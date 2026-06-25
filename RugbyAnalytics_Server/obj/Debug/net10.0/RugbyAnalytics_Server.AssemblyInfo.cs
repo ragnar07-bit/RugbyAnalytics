889c77380bcea820c0c4933faeea075c4e8d48d9
ragnar07-bit <programmazione.moderna@gmail.com>
2026-06-25 18:00:08 +0200

    feat(server): implementati moduli di crittografia e ascolto socket TCP            Creato CryptographyService con SHA-256, salting sicuro e protezione da timing attack.            Creato SocketListener asincrono per la gestione multithreading dei client.

--- RugbyAnalytics_Server/obj/Debug/net10.0/RugbyAnalytics_Server.AssemblyInfo.cs
+++ RugbyAnalytics_Server/obj/Debug/net10.0/RugbyAnalytics_Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RugbyAnalytics_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df8a94a9d626ee40d75dab1983f810a0647824b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8162322b829b7992324a850e9c46f33fa08ec138")]
 [assembly: System.Reflection.AssemblyProductAttribute("RugbyAnalytics_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RugbyAnalytics_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
